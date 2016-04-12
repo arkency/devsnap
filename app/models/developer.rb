9270f3a47fc87ba8ff97d1908f5533a74f00ee81
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2016-04-12 11:16:04 +0200

    hotfixing

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,8 +1,4 @@
 class Developer < ApplicationRecord
-<<<<<<< HEAD
-  validates :snapchat_username, :full_name, :about, presence: true
-=======
   validates :login, :full_name, :about, presence: true
   scope :from_newest, -> { order('created_at DESC') }
->>>>>>> upstream/master
 end
