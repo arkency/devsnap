f851aba4a88e8fe0030abf8743120db0c2884c01
Emanuel <siawmensah@gmail.com>
2016-04-14 15:44:15 +0200

    Change default error message

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,5 +1,5 @@
 class Developer < ApplicationRecord
   validates :snapchat_username, :full_name, :about, presence: true
-  validates :snapchat_username, uniqueness: true
+  validates :snapchat_username, uniqueness: { message: "is already in this directory."}
   scope :from_newest, -> { order('created_at DESC') }
 end
