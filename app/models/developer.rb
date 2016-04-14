1b6caf4fd21d6970cf4d591b853b6b1c26f8ac37
Emanuel <siawmensah@gmail.com>
2016-04-14 15:08:32 +0200

    Prevent users from re-adding themselves

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,4 +1,5 @@
 class Developer < ApplicationRecord
   validates :snapchat_username, :full_name, :about, presence: true
+  validates :snapchat_username, uniqueness: true
   scope :from_newest, -> { order('created_at DESC') }
 end
