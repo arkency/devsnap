43c3fd14669e7259712253dd9d7a82804f5fab98
Emanuel <siawmensah@gmail.com>
2016-04-19 00:09:13 +0200

    Remove disturbing comments

--- db/schema.rb
+++ db/schema.rb
@@ -1,19 +1,4 @@
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 20160418211433) do
-
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "developers", force: :cascade do |t|
@@ -28,5 +13,4 @@ ActiveRecord::Schema.define(version: 20160418211433) do
     t.datetime "snapcode_updated_at"
     t.string   "snapcode_image"
   end
-
 end
