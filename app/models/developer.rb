8dfa7a33bfaf739f592137f7cf312812577c5609
Emanuel <siawmensah@gmail.com>
2016-04-21 13:51:34 +0200

    Delegate downloading of svg images

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,7 +1,4 @@
-
-require_relative '../services/snapchat_svg_service'
 class Developer < ApplicationRecord
-	include SnapchatSvgService
 	before_save :get_svg_image
 
   validates :snapchat_username, :full_name, :about, presence: true
@@ -11,7 +8,6 @@ class Developer < ApplicationRecord
   private
 
   def get_svg_image
-		svg = SnapchatSvgService.get_svg_image("#{ self.snapchat_username }")
-		self.snapcode_image = svg
+		self.snapcode_image = SnapchatSvgService.get_svg_image("#{ self.snapchat_username }")
 	end
 end
--- app/services/snapchat_svg_service.rb
+++ app/services/snapchat_svg_service.rb
@@ -5,7 +5,8 @@ BASE_URL = "https://feelinsonice-hrd.appspot.com/web/deeplink/snapcode?username=
 
 module SnapchatSvgService
 	def get_svg_image(username)
-		image = Nokogiri::XML open(BASE_URL.gsub(/snapusername/, 'username'))
+		image = Nokogiri::XML open(BASE_URL.gsub(/snapusername/, "#{ username }"))
 		image.to_html
 	end
+	module_function :get_svg_image
 end
