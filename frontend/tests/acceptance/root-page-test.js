1d4ee4697f030bb6e0d9007d009d079066bd4e55
Mariusz Zak <mariusz.zak@ironin.pl>
2016-07-09 11:54:21 +0200

    Add developer factory. Cleanup root-page-test.js.

--- frontend/tests/acceptance/root-page-test.js
+++ frontend/tests/acceptance/root-page-test.js
@@ -3,6 +3,19 @@ import moduleForAcceptance from 'devsnap/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | root page');
 
+const helpers = {
+  createRobocop() {
+    return server.create('developer', {
+      snapchatUsername: 'robocop',
+      fullName: 'Robo Cop',
+      about: 'Pif Paf'
+    });
+  },
+  clickOnFirstSnapchatUsername() {
+    click('.snapchat-username:first a');
+  }
+};
+
 test('visiting /', function(assert) {
   visit('/');
 
@@ -13,26 +26,35 @@ test('visiting /', function(assert) {
 });
 
 test('displaying a table with developers', function(assert) {
+  helpers.createRobocop();
+  server.createList('developer', 3);
   visit('/');
 
   andThen(function() {
-    assert.equal(find('table tbody .snapchat-username').length, 3 );
-    assert.equal(find('table tbody tr:first td:nth-child(1)').text().trim(), 'andrzejkrzywda' );
-    assert.equal(find('table tbody tr:first td:nth-child(2)').text().trim(), 'Andrzej Krzywda' );
-    assert.equal(find('table tbody tr:first td:nth-child(3)').text().trim(), 'Ruby, Rails, JavaScript, React, legacy, CEO, remote work, DDD' );
+    assert.equal(find('table tbody .snapchat-username').length, 4 );
+    assert.equal(find('table tbody tr:first td:nth-child(1)').text().trim(), 'robocop' );
+    assert.equal(find('table tbody tr:first td:nth-child(2)').text().trim(), 'Robo Cop' );
+    assert.equal(find('table tbody tr:first td:nth-child(3)').text().trim(), 'Pif Paf' );
   });
 });
 
 test('opening a modal after clicking on snapchat username', function(assert) {
+  const robocop = helpers.createRobocop();
   visit('/');
 
-  click('.snapchat-username:first a');
+  helpers.clickOnFirstSnapchatUsername();
   andThen(function() {
     assert.equal(find('.ember-modal-dialog').length, 1);
-    assert.equal(find('h1').text().trim(), 'Andrzej Krzywda');
-    assert.equal(currentURL(), '/developers/1');
+    assert.equal(find('h1').text().trim(), robocop.fullName);
+    assert.equal(currentURL(), '/developers/' + robocop.id);
   });
+});
 
+test('closing a modal after clicking on close button', function(assert) {
+  helpers.createRobocop();
+  visit('/');
+
+  helpers.clickOnFirstSnapchatUsername();
   click('.ember-modal-dialog button');
   andThen(function() {
     assert.equal(find('.ember-modal-dialog').length, 0);
@@ -40,3 +62,14 @@ test('opening a modal after clicking on snapchat username', function(assert) {
   });
 });
 
+test('closing a modal after clicking on overlay shadow', function(assert) {
+  helpers.createRobocop();
+  visit('/');
+
+  helpers.clickOnFirstSnapchatUsername();
+  click('.ember-modal-overlay');
+  andThen(function() {
+    assert.equal(find('.ember-modal-dialog').length, 0);
+    assert.equal(currentURL(), '/');
+  });
+});
