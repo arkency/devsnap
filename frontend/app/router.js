c401618472c887881a495e735df9c36c4ad51cce
Mariusz Zak <mariusz.zak@ironin.pl>
2016-07-09 18:09:20 +0200

    - Add some CSS, basic responsiveness - Updated bootstrap

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -8,7 +8,9 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('developers', { path: '/' }, function() {
     this.route('show', { path: '/developers/:developerId' });
+    this.route('new');
   });
+  this.route('codes');
 });
 
 export default Router;
