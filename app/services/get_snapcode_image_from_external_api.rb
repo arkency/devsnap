b7b2a37f6909bbc26077af4740b201edd91ce76f
Mariusz Zak <mariusz.zak@ironin.pl>
2016-07-24 13:23:30 +0200

    Change tabs to double spaces

--- app/services/get_snapcode_image_from_external_api.rb
+++ app/services/get_snapcode_image_from_external_api.rb
@@ -4,16 +4,16 @@ require 'open-uri'
 class GetSnapcodeImageFromExternalApi
   FetchingFailed = Class.new(StandardError)
 
-	def call(snapchat_username)
-		image = Nokogiri::XML(open(api_url_for_image(snapchat_username)))
-		image.to_html
+  def call(snapchat_username)
+    image = Nokogiri::XML(open(api_url_for_image(snapchat_username)))
+    image.to_html
   rescue OpenURI::HTTPError
     raise FetchingFailed
-	end
+  end
 
-	private
+  private
 
-	def api_url_for_image(username)
-		"https://feelinsonice-hrd.appspot.com/web/deeplink/snapcode?username=#{username}&type=SVG"
-	end
+  def api_url_for_image(username)
+    "https://feelinsonice-hrd.appspot.com/web/deeplink/snapcode?username=#{username}&type=SVG"
+  end
 end
